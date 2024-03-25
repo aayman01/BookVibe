57cae64f83ff7006391a54682331dab8324b8f2a
aayman01 <ayman.alfi098@gmail.com>
2024-03-26 02:48:04 +0600

    homepage responsive done

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,27 +1,36 @@
+import { CiStar } from "react-icons/ci";
+import PropTypes from "prop-types";
 
-const Book = ({book}) => {
-    const { image, tags, author, bookName, category, rating } = book;
-    return (
-      <div className="card card-compact  bg-base-100 p-4 border">
-        <figure className="bg-[#F3F3F3] rounded-xl py-4">
-          <img src={image} alt="Books" />
-        </figure>
-        <div className="flex items-center gap-4 p-4 ">
-          {tags.map((tag, idx) => (
-            <p key={idx}>{tag}</p>
-          ))}
-        </div>
-        <div className="card-body">
-          <h2 className="card-title">{bookName}</h2>
-          <p>By : {author}</p>
-          <hr />
-        </div>
-        <div className="flex justify-between p-4">
-          <p>{category}</p>
-          <p>{rating}</p>
+const Book = ({ book }) => {
+  const { image, tags, author, bookName, category, rating } = book;
+  return (
+    <div className="card card-compact  bg-base-100 p-4 border">
+      <figure className="bg-[#F3F3F3] rounded-xl py-4">
+        <img src={image} alt="Books" />
+      </figure>
+      <div className="flex items-center gap-3 p-4 ">
+        {tags.map((tag, idx) => (
+          <p key={idx} className="bg-green-200 text-[#23BE0A] text-sm rounded-xl px-4 py-1">
+            {tag}
+          </p>
+        ))}
+      </div>
+      <div className="card-body">
+        <h2 className="card-title text-2xl font-bold">{bookName}</h2>
+        <p className="text-base font-medium mb-3">By : {author}</p>
+        <hr />
+      </div>
+      <div className="flex justify-between p-4">
+        <p className="font-normal text-base">{category}</p>
+        <div className="flex items-center gap-1">
+          {rating}
+          <CiStar />
         </div>
       </div>
-    );
+    </div>
+  );
 };
-
+Book.propTypes = {
+    book : PropTypes.object.isRequired
+}
 export default Book;
--- src/Components/BookList/BookList.jsx
+++ src/Components/BookList/BookList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Book from "../Book/Book";
 
+
 const BookList = () => {
     const [books, setBooks] = useState([]);
 
@@ -12,7 +13,7 @@ const BookList = () => {
     return (
       <div className="mt-24 mb-10">
         <h2 className="text-center text-[#131313] font-bold text-4xl mb-10">Books</h2>
-        <div className="grid grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {books.map((book) => (
             <Book key={book.id} book={book}></Book>
           ))}
